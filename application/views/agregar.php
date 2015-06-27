c65173d12ef10cc08b76324d5b2d0d19c6530ee3
Fernando Vera <ferverare@gmail.com>
2015-06-27 14:55:00 -0500

    Guardando Cambios

--- application/views/agregar.php
+++ application/views/agregar.php
@@ -2,38 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <title>Productos</title>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="http://localhost/codeigniter/css/foundation.css" />
-    <link rel="stylesheet" href="http://localhost/codeigniter/css/foundation.min.css" />
-    <link rel="stylesheet" href="http://localhost/codeigniter/css/normalize.css" />
-
-    <script src="http://localhost/codeigniter/js/fondation.min.js"></script>
-  
-    <script src="http://localhost/codeigniter/js/vendor/modernizr.js"></script>
-    <script src="http://localhost/codeigniter/js/vendor/fastclick.js"></script>
-    <script src="http://localhost/codeigniter/js/vendor/jquery.cookie.js"></script>
-    <script src="http://localhost/codeigniter/js/vendor/jquery.js"></script>
-    <script src="jhttp://localhost/codeigniter/s/vendor/placeholder.js"></script>
-
-    <script src="http://localhost/codeigniter/js/foundation/foundation.abide.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.accordion.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.alert.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.clearing.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.dropdow.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.equalizer.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.interchange.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.joyride.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.magellan.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.offcanvas.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.orbit.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.reveal.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.slider.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.tab.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.tooltip.js"></script>
-    <script src="http://localhost/codeigniter/js/foundation/foundation.topbar.js"></script>
-
+     <?=$this->load->view('header/style');?>
   </head>
   <body>
 
